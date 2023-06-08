b6ffb05ccc2eb0719745aa4de22f6c9da39987b4
Andrea Liliana Griffiths <andreagriffiths11@github.com>
2023-06-08 11:40:06 -0400

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,46 @@
+# Changelog
+
+## [1.1.0](https://github.com/icelam/random-name-picker/compare/v1.0.4...v1.1.0) (2022-02-28)
+
+
+### Features
+
+* refine animation with blur filter ([1025350](https://github.com/icelam/random-name-picker/commit/10253508457fd05e9a4ea8c0a005fb0a7962f817))
+
+
+### Bug Fixes
+
+* fix maximum number of reel item when previous winner is shown ([70994da](https://github.com/icelam/random-name-picker/commit/70994daef788c5545caeb5e778c6c3e25c6a6150))
+* fix page overflow when sunburst is displayed ([8e1c167](https://github.com/icelam/random-name-picker/commit/8e1c167c81fd68c8756f2826bd20a48d1555bf71))
+* fix the size of og-image ([b5a904e](https://github.com/icelam/random-name-picker/commit/b5a904ed70e465a34774b97662a16175304d0b16))
+
+### [1.0.4](https://github.com/icelam/random-name-picker/compare/v1.0.3...v1.0.4) (2021-03-19)
+
+
+### Bug Fixes
+
+* fix page overflow when sunburst is displayed ([b279b34](https://github.com/icelam/random-name-picker/commit/b279b34f8088288b443f302dda7e513caf90f900))
+
+### [1.0.3](https://github.com/icelam/random-name-picker/compare/v1.0.2...v1.0.3) (2021-02-22)
+
+### [1.0.2](https://github.com/icelam/random-name-picker/compare/v1.0.1...v1.0.2) (2021-02-21)
+
+
+### Bug Fixes
+
+* clear winner and winning animation when updating name list ([b53c2cb](https://github.com/icelam/random-name-picker/commit/b53c2cb321f92526d49726773a0604e7b30a1613))
+* remove the use of commitStyles ([45f1796](https://github.com/icelam/random-name-picker/commit/45f17964c4e0cf6aa2948815484d9f35f82c453b))
+
+### [1.0.1](https://github.com/icelam/random-name-picker/compare/v1.0.0...v1.0.1) (2021-02-21)
+
+
+### Bug Fixes
+
+* bottom padding of setting panel is not show on firefox on content overflow ([2b8c50f](https://github.com/icelam/random-name-picker/commit/2b8c50f42fc064f05ac1ec552390aa36d9d4b0cc))
+
+## 1.0.0 (2021-02-21)
+
+
+### Features
+
+* initial version of random name picker ([5be5b0d](https://github.com/icelam/random-name-picker/commit/5be5b0d5117ffde1180f30f3d3c3f3645193ea19))
---
+++ src/assets/js/Slot.ts
@@ -0,0 +1,228 @@
+interface SlotConfigurations {
+  /** User configuration for maximum item inside a reel */
+  maxReelItems?: number;
+  /** User configuration for whether winner should be removed from name list */
+  removeWinner?: boolean;
+  /** User configuration for element selector which reel items should append to */
+  reelContainerSelector: string;
+  /** User configuration for callback function that runs before spinning reel */
+  onSpinStart?: () => void;
+  /** User configuration for callback function that runs after spinning reel */
+  onSpinEnd?: () => void;
+
+  /** User configuration for callback function that runs after user updates the name list */
+  onNameListChanged?: () => void;
+}
+
+/** Class for doing random name pick and animation */
+export default class Slot {
+  /** List of names to draw from */
+  private nameList: string[];
+
+  /** Whether there is a previous winner element displayed in reel */
+  private havePreviousWinner: boolean;
+
+  /** Container that hold the reel items */
+  private reelContainer: HTMLElement | null;
+
+  /** Maximum item inside a reel */
+  private maxReelItems: NonNullable<SlotConfigurations['maxReelItems']>;
+
+  /** Whether winner should be removed from name list */
+  private shouldRemoveWinner: NonNullable<SlotConfigurations['removeWinner']>;
+
+  /** Reel animation object instance */
+  private reelAnimation?: Animation;
+
+  /** Callback function that runs before spinning reel */
+  private onSpinStart?: NonNullable<SlotConfigurations['onSpinStart']>;
+
+  /** Callback function that runs after spinning reel */
+  private onSpinEnd?: NonNullable<SlotConfigurations['onSpinEnd']>;
+
+  /** Callback function that runs after spinning reel */
+  private onNameListChanged?: NonNullable<SlotConfigurations['onNameListChanged']>;
+
+  /**
+   * Constructor of Slot
+   * @param maxReelItems  Maximum item inside a reel
+   * @param removeWinner  Whether winner should be removed from name list
+   * @param reelContainerSelector  The element ID of reel items to be appended
+   * @param onSpinStart  Callback function that runs before spinning reel
+   * @param onNameListChanged  Callback function that runs when user updates the name list
+   */
+  constructor(
+    {
+      maxReelItems = 30,
+      removeWinner = true,
+      reelContainerSelector,
+      onSpinStart,
+      onSpinEnd,
+      onNameListChanged
+    }: SlotConfigurations
+  ) {
+    this.nameList = [];
+    this.havePreviousWinner = false;
+    this.reelContainer = document.querySelector(reelContainerSelector);
+    this.maxReelItems = maxReelItems;
+    this.shouldRemoveWinner = removeWinner;
+    this.onSpinStart = onSpinStart;
+    this.onSpinEnd = onSpinEnd;
+    this.onNameListChanged = onNameListChanged;
+
+    // Create reel animation
+    this.reelAnimation = this.reelContainer?.animate(
+      [
+        { transform: 'none', filter: 'blur(0)' },
+        { filter: 'blur(1px)', offset: 0.5 },
+        // Here we transform the reel to move up and stop at the top of last item
+        // "(Number of item - 1) * height of reel item" of wheel is the amount of pixel to move up
+        // 7.5rem * 16 = 120px, which equals to reel item height
+        { transform: `translateY(-${(this.maxReelItems - 1) * (7.5 * 16)}px)`, filter: 'blur(0)' }
+      ],
+      {
+        duration: this.maxReelItems * 100, // 100ms for 1 item
+        easing: 'ease-in-out',
+        iterations: 1
+      }
+    );
+
+    this.reelAnimation?.cancel();
+  }
+
+  /**
+   * Setter for name list
+   * @param names  List of names to draw a winner from
+   */
+  set names(names: string[]) {
+    this.nameList = names;
+
+    const reelItemsToRemove = this.reelContainer?.children
+      ? Array.from(this.reelContainer.children)
+      : [];
+
+    reelItemsToRemove
+      .forEach((element) => element.remove());
+
+    this.havePreviousWinner = false;
+
+    if (this.onNameListChanged) {
+      this.onNameListChanged();
+    }
+  }
+
+  /** Getter for name list */
+  get names(): string[] {
+    return this.nameList;
+  }
+
+  /**
+   * Setter for shouldRemoveWinner
+   * @param removeWinner  Whether the winner should be removed from name list
+   */
+  set shouldRemoveWinnerFromNameList(removeWinner: boolean) {
+    this.shouldRemoveWinner = removeWinner;
+  }
+
+  /** Getter for shouldRemoveWinner */
+  get shouldRemoveWinnerFromNameList(): boolean {
+    return this.shouldRemoveWinner;
+  }
+
+  /**
+   * Returns a new array where the items are shuffled
+   * @template T  Type of items inside the array to be shuffled
+   * @param array  The array to be shuffled
+   * @returns The shuffled array
+   */
+  private static shuffleNames<T = unknown>(array: T[]): T[] {
+    const keys = Object.keys(array) as unknown[] as number[];
+    const result: T[] = [];
+    for (let k = 0, n = keys.length; k < array.length && n > 0; k += 1) {
+      // eslint-disable-next-line no-bitwise
+      const i = Math.random() * n | 0;
+      const key = keys[i];
+      result.push(array[key]);
+      n -= 1;
+      const tmp = keys[n];
+      keys[n] = key;
+      keys[i] = tmp;
+    }
+    return result;
+  }
+
+  /**
+   * Function for spinning the slot
+   * @returns Whether the spin is completed successfully
+   */
+  public async spin(): Promise<boolean> {
+    if (!this.nameList.length) {
+      console.error('Name List is empty. Cannot start spinning.');
+      return false;
+    }
+
+    if (this.onSpinStart) {
+      this.onSpinStart();
+    }
+
+    const { reelContainer, reelAnimation, shouldRemoveWinner } = this;
+    if (!reelContainer || !reelAnimation) {
+      return false;
+    }
+
+    // Shuffle names and create reel items
+    let randomNames = Slot.shuffleNames<string>(this.nameList);
+
+    while (randomNames.length && randomNames.length < this.maxReelItems) {
+      randomNames = [...randomNames, ...randomNames];
+    }
+
+    randomNames = randomNames.slice(0, this.maxReelItems - Number(this.havePreviousWinner));
+
+    const fragment = document.createDocumentFragment();
+
+    randomNames.forEach((name) => {
+      const newReelItem = document.createElement('div');
+      newReelItem.innerHTML = name;
+      fragment.appendChild(newReelItem);
+    });
+
+    reelContainer.appendChild(fragment);
+
+    console.log('Displayed items: ', randomNames);
+    console.log('Winner: ', randomNames[randomNames.length - 1]);
+
+    // Remove winner form name list if necessary
+    if (shouldRemoveWinner) {
+      this.nameList.splice(this.nameList.findIndex(
+        (name) => name === randomNames[randomNames.length - 1]
+      ), 1);
+    }
+
+    console.log('Remaining: ', this.nameList);
+
+    // Play the spin animation
+    const animationPromise = new Promise((resolve) => {
+      reelAnimation.onfinish = resolve;
+    });
+
+    reelAnimation.play();
+
+    await animationPromise;
+
+    // Sets the current playback time to the end of the animation
+    // Fix issue for animatin not playing after the initial play on Safari
+    reelAnimation.finish();
+
+    Array.from(reelContainer.children)
+      .slice(0, reelContainer.children.length - 1)
+      .forEach((element) => element.remove());
+
+    this.havePreviousWinner = true;
+
+    if (this.onSpinEnd) {
+      this.onSpinEnd();
+    }
+    return true;
+  }
+}
